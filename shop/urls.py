fff552b574719866ff46db359ebb8afc22fc7f80
zerojooon <jun041577@snu.ac.kr>
2020-07-22 14:22:36 +0900

    3-5강

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path, register_converter
 from shop import views
 
 app_name = 'shop'
 
+register_converter(FourDigitYearConverter,'yyyy')
+
 urlpatterns = [
-    # path('',views ),
-    path('shop/', views.item_list),
+    path('', views.item_list, name='item_list'),
+    path('shop/', views.item_list, name='item_new'),
+    path('<int:id>/edit/', views.item_edit, name='item_edit'),  # Item수정
+    path('<int:id>/delete/', views.item_delete, name='item_delete'),  # Item삭제
+    path('<int:id>/reviews/', views.review_list, name='review_list'),  # 리뷰목록
+    path('<int:item_id>/reviews/<int:id>/edit/$', views.review_edit,  # 리뷰수정
+         name='review_edit'),
+    path('<int:item_id>/reviews/<int:id>/delete/$', views.review_delete, name='review_delete'),
 ]
