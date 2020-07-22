7295b38bacb015f7de215f947a28648b7b95cb4a
zerojooon <jun041577@snu.ac.kr>
2020-07-23 00:33:40 +0900

    12강

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+#1
+# admin.site.register(Item)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk','name','price','short_desc','is_publish']
+    list_display_links = ['name']
+    search_fields = ['name']
+    list_filter = ['is_publish','updated_at']
+    def short_desc(self,item):
+        return item.desc[:20]
+
--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,13 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f'<{self.pk}> {self.name}'
 
 
