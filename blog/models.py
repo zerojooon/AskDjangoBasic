667985ffb98807a4b28be781a510b9d9456493de
zerojooon <jun041577@snu.ac.kr>
2020-07-23 02:41:11 +0900

    14-16강

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
