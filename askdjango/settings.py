1d1879e897067570c3c5f146d0bc3d94542898a5
zerojooon <jun041577@snu.ac.kr>
2020-07-24 15:35:55 +0900

    23-25강

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -145,6 +145,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'askdjango', 'static')
+]
+
+
+
 INTERNAL_IPS = ['127.0.0.1']
 
 LOGGING = {
