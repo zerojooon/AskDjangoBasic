f42e0716b94d803eccccd9996533e80f654dec17
zerojooon <jun041577@snu.ac.kr>
2020-07-24 17:41:56 +0900

    26강

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from askdjango.utils import uuid_name_upload_to
 
 # Create your models here.
 
 class Post(models.Model):
-    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    photo = models.ImageField(upload_to=uuid_name_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from askdjango.utils import uuid_name_upload_to
+
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    photo = models.ImageField(blank=True, upload_to=uuid_name_upload_to)
     is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
